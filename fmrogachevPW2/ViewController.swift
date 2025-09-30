1d6ded661b2f21a4945b3b5856999b41806de033
Felix <rogachev.feliks@yandex.ru>
2025-09-30 18:45:27 +0300

    Initial Commit

---
+++ fmrogachevPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fmrogachevPW2
+//
+//  Created by Feliks Rogachev on 30.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
